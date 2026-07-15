837ce45632f964f5f0e2b6aee216193ad9ba2d83
vipinrajvr1992 <vipinrajvr1992@gmail.com>
2026-07-15 14:31:11 +0530

    Update config.js

--- admin/js/config.js
+++ admin/js/config.js
@@ -1,4 +1,4 @@
 const CONFIG = {
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwlw80uUTgLIKP16PdYdnYTY9NpqVoZmPqfq-KN58WDOoyrXZgDDLFLPjIa8YLSPHdO/exec",
+    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbykL_Vp67Zft8VuWB4rpxdAgopWGPx0gkAic49lYayzP3kv707TcM1M1N3aPmymhTo1/exec",
     SESSION_KEY: "munambam_admin"
 };
