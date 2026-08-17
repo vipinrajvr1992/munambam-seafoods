49c31515b788043860b55f6e9f415e62eba58639
vipinrajvr1992 <vipinrajvr1992@gmail.com>
2026-08-17 17:13:22 +0530

    Delete admin directory

--- admin/js/config.js
+++
@@ -1,8 +0,0 @@
-// ==========================================
-// CONFIG.JS - Fully Updated
-// ==========================================
-
-const CONFIG = {
-    SCRIPT_URL: "https://script.google.com/macros/s/AKfycbwg9NUisn3ukg_s3OtQ_5ol9L5jI7c2n-99OsfQwsbgY_5BG3KDSwhgecUmeavVodPz/exec",
-    SESSION_KEY: "munambam_admin"
-};
